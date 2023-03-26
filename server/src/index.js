0c38ce5d72fc715aac3d982be752134fb34c6602
Raju George <admin@goingcloudnative.com>
2023-03-26 17:14:45 +0530

    completed the initial version of todos

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import todoRouter from './todoRouter.js'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
 app.get('/', async (req, res) => {
-    res.send("default route")
+    res.send("default route dibe")
 })
 
 app.use('/todos', todoRouter)
--- server/src/todoRouter.js
+++ server/src/todoRouter.js
@@ -5,7 +5,43 @@ const router = Router();
 
 router.get('/', async (req, res) => {
     const alltodos = await prisma.todo.findMany()
-    res.send(alltodos)
+    res.status(200).json(alltodos)
+})
+
+router.post('/', async (req, res) => {
+    const todo = await prisma.todo.create({
+        data: req.body
+    })
+
+    res.status(201).json(todo)
+})
+
+router.get('/:id', async (req, res) => {
+    const todo = await prisma.todo.findUnique({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json(todo)
+})
+
+router.put('/:id', async (req, res) => {
+    const todo = await prisma.todo.update({
+        where: {
+            id: req.params.id
+        },
+        data: req.body
+    })
+    res.status(200).json(todo)
+})
+
+router.delete('/:id', async (req, res) => {
+    const todo = await prisma.todo.delete({
+        where: {
+            id: req.params.id,
+        },
+    });
+    res.status(200).json(todo)
 })
 
 
