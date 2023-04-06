89f89411db480fe87aa8e584d4330291cf859e53
rajuplayground <rajudevbox@gmail.com>
2023-04-06 12:45:02 +0530

    made frontend app a ts app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,9 @@ import GlobalStyles from "./components/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <div>
+      <h1>hellow</h1>
+    </div> */}
     <App />
     <GlobalStyles />
   </React.StrictMode>
